28dfae1314f55297f312e158e8709d4fdd4eef86
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-20 22:15:06 +0800

    Update Coba.py

--- Coba.py
+++ Coba.py
@@ -1,3 +1,4 @@
+
 import random
 import sys
 import time
@@ -7,12 +8,5 @@ def mengetik(s):
         sys.stdout.flush()
 #kecepatan mengetik
         time.sleep(random.random() * 0.1)import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-      mengetik ('DI COBA JANHAN DI COBA COBA JO ')
+
+mengetik ('SALA SALA SLA SLALAKALAMA')
