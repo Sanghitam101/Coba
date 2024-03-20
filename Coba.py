83d6cef54abce194defd35011b6fbf14efd846bb
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-20 22:09:43 +0800

    Create Coba.py

---
+++ Coba.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+      mengetik ('DI COBA JANHAN DI COBA COBA JO ')
